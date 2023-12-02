01f020ba0a71d14fb41d93e8a20c5726b70514d6
Adam Łuszcz <adam.luszcz20@gmail.com>
2023-12-02 15:07:35 +0100

    Change class name

--- Laboratorium_2/tests/selenium_tests.py
+++ Laboratorium_2/tests/selenium_tests.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-class PythonOrgSearch(unittest.TestCase):
+class SeleniumTests(unittest.TestCase):
 
     def setUp(self):
         self.drivers = [webdriver.Chrome(), webdriver.Firefox()]
